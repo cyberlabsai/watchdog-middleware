3cda8c76eb398a8ddfae748170d38c0bdf111328
Lucca <luccabuffara@hotmail.com>
2019-08-24 10:38:14 -0300

    Add modelo NLP

--- app.py
+++ app.py
@@ -2,6 +2,30 @@ from flask import Flask, jsonify, request
 from db import updateTweetRead, getTweets
 import Tweet
 
+from sklearn.externals import joblib
+
+class NLP:
+
+        def __init__(self):
+                self.classifier = joblib.load("modelo.pkl")
+                self.vectorizer = joblib.load("vectorizer.pkl")
+
+        def classify(self, texts):
+                """Retorna se comentario e ofensivo ou nao
+
+                Arguments
+                ---------
+                texts: numpy.ndarray
+                Lista contendo as frases/tweets eg: texts = ["Brazil JS 'e o bicho"]
+
+                Returns
+                -------
+                results: list
+                Lista numpy contendo 0 para se frase nao e ofensiva 1 se ela for
+                """
+
+                freq = vectorizer_loaded.transform(testes)
+                return classifier.predict(freq)
 
 app = Flask(__name__)
 
